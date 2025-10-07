a45ca7a6e6d9613190784c21e029d69ef96d0674
APT-Rank <aptranking@gmail.com>
2025-10-07 22:44:36 +0900

    앱 다운로드 압내 팝업 이미지 경로 수정

--- js/share.js
+++ js/share.js
@@ -924,7 +924,7 @@ function setAppDownloadModal(){
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content" id="appDownloadModaloutline">
           <div class="modal-body" id="appDownloadModalBody">
-            <div><img src="./apt-rank-152x152.png" width="70px" style="border-radius: 10px;"></div>
+            <div><img src="https://www.realrankus.com/apt-rank-152x152.png" width="70px" style="border-radius: 10px;"></div>
             <div id="appDownloadModalDescription">리얼랭커스 앱을 설치하시면,<br>더 넓은 화면으로 볼 수 있어요!
             <br><span style='font-size: 0.75em; color: #999;'>(앱 크기가 5메가도 안 돼요!)</span></div>            
             <div class="app-download-link" onClick="openOuterLink('${appDownloadLink}')">앱 설치하기</div>
