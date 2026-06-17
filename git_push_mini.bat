9b45cec3f93dbcb9413ee95e17f2dba3cfedd214
APT-Rank <aptranking@gmail.com>
2026-06-17 09:16:26 +0900

    Real-Price Auto Update

--- git_push_mini.bat
+++ git_push_mini.bat
@@ -4,4 +4,6 @@ git config --global core.autocrlf true
 git config --global credential.helper manager-core
 git add *
 git commit -m "Real-Price Auto Update"
-git push -u origin master
+git push -u origin master --force
+
+pause
