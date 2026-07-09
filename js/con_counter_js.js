c5d0028a5cacb4b6fb6c80f4fb6bf65df8dda396
APT-Rank <aptranking@gmail.com>
2026-07-09 21:07:53 +0900

    Firebase key 수정

--- js/con_counter_js.js
+++ js/con_counter_js.js
@@ -1,15 +1,17 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyA7s95oaj498XdArjo9cT_8watLVw4JK3M",
-	authDomain: "aptrank-cc61b.firebaseapp.com",
-	databaseURL: "https://aptrank-cc61b-default-rtdb.firebaseio.com",
-	projectId: "aptrank-cc61b",
-	storageBucket: "aptrank-cc61b.appspot.com",
-	messagingSenderId: "987401326011",
-	appId: "1:987401326011:web:8732d04a9fc69280d7489e",
-	measurementId: "G-BH5DRBH380"
+  apiKey: "AIzaSyAivJVoqhrDQzuslEAoizhb5ByGhpQGHzE",
+  authDomain: "aptrank-cc61b.firebaseapp.com",
+  databaseURL: "https://aptrank-cc61b-default-rtdb.firebaseio.com",
+  projectId: "aptrank-cc61b",
+  storageBucket: "aptrank-cc61b.firebasestorage.app",
+  messagingSenderId: "987401326011",
+  appId: "1:987401326011:web:d5a08de82a2aa2aed7489e",
+  measurementId: "G-LSBHV7N1ND"
 };
 
 const default_db = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(default_db);
+
 const sub01_db = firebase.initializeApp({
     databaseURL: "https://realrankus-sub01.firebaseio.com/"
 }, 'sub01_db');
